83d8e94714f46f18346212c5b7d9b41349e10972
AnnaAplavina <59871896+AnnaAplavina@users.noreply.github.com>
2025-05-15 10:52:14 +0300

    not using short reviews for statistics in reports

--- src/main/java/com/aplavina/reviewcheckbot/scheduler/FileCheckReportScheduler.java
+++ src/main/java/com/aplavina/reviewcheckbot/scheduler/FileCheckReportScheduler.java
@@ -6,6 +6,7 @@ import com.aplavina.reviewcheckbot.repository.FileCheckRepository;
 import com.aplavina.reviewcheckbot.service.bot.TelegramBotService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -20,6 +21,9 @@ public class FileCheckReportScheduler {
     private final FileCheckRepository fileCheckRepository;
     private final TelegramBotService telegramBotService;
 
+    @Value("${min-review-text-length}")
+    private int minTextLength;
+
     @Async("schedulerThreadPool")
     @Transactional
     @Scheduled(cron = "${scheduler.reports.cron}", zone = "${scheduler.reports.zone}")
@@ -29,6 +33,7 @@ public class FileCheckReportScheduler {
             log.info("Preparing report for file check with file key: {}", fileCheck.getFileKey());
             List<ReviewCheck> reviewChecks = fileCheck.getReviewChecks();
             double avgFakeScore = reviewChecks.stream()
+                    .filter(check -> check.getText().split(" ").length >= minTextLength)
                     .mapToDouble(ReviewCheck::getFakeScorePercentage)
                     .average()
                     .orElse(0.0);
@@ -39,8 +44,9 @@ public class FileCheckReportScheduler {
                         .count() * 100.0 / reviewChecks.size();
             }
             String report = String.format("Fake score: %.2f%%\n" +
-                            "Percent of fake reviews: %.2f%%",
-                    avgFakeScore, percentFake);
+                            "Percent of fake reviews: %.2f%%\n" +
+                            "Reviews with text length less than %d words were not used for statistics",
+                    avgFakeScore, percentFake, minTextLength);
             telegramBotService.sendMessage(fileCheck.getChatId(), report);
             fileCheckRepository.delete(fileCheck);
         }
